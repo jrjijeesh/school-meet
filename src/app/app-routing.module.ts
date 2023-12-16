2b19937519972f7b6318f88306d118650aba97c2
Jijeesh <jijeeshjr@bitbucket.org>
2023-12-16 18:45:56 +0530

    Signup added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContainerComponent } from './layout/container/container.component';
 import { UserLoginComponent } from './user-access/user-login/user-login.component';
+import { UserSignupComponent } from './user-access/user-signup/user-signup.component';
 
 const routes: Routes = [
   { path: 'login', component: UserLoginComponent },
+  { path: 'signup', component: UserSignupComponent },
   {
     path: 'app',
     component: ContainerComponent,
--- src/app/user-access/user-access.module.ts
+++ src/app/user-access/user-access.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserSignupComponent } from './user-signup/user-signup.component';
 
 @NgModule({
-  declarations: [UserLoginComponent],
+  declarations: [UserLoginComponent, UserSignupComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class UserAccessModule {}
